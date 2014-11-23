b61c90b94ceedbd5a845d6ecf0e1b643ecb9bd24
Dan Pasanen <dan.pasanen@gmail.com>
2014-11-23 16:02:03 -0600

    [WIP] sepolicy: add device-specific rules
    Change-Id: I025d5f44d0b3dc710bd9bc6a44aee27a63a68136

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,12 +85,25 @@ BOARD_SEPOLICY_DIRS += device/htc/dlx/sepolicy
 
 BOARD_SEPOLICY_UNION += \
     akmd.te \
+    device.te \
+    drmserver.te \
     file_contexts \
     hcheck.te \
+    init.te \
+    init_shell.te \
     kickstart.te \
+    mediaserver.te \
+    mm-qcamerad \
+    mpdecision.te \
+    netmgrd.te \
+    qcks.te \
+    qmuxd.te \
     qrngd.te \
+    surfaceflinger.te \
     sysinit.te \
-    thermald.te
+    tee.te \
+    thermald.te \
+    ueventd.te
 
 # USB
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
