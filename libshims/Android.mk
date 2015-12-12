3543eb606df6ff19b0ff54b4829065f129284321
Dan Pasanen <invisiblek@cyanogenmod.org>
2015-12-11 21:26:31 -0600

    libshims: bust off vector impl symbols
    * link it in for camera shim
    * shim it in for sensor hal
    
    Change-Id: I87915433d884da65032ea5db4251e9bfd4cd0868

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -1,5 +1,17 @@
 LOCAL_PATH := $(call my-dir)
 
+include $(CLEAR_VARS)
+
+# vector impl symbols
+
+LOCAL_SRC_FILES := \
+     VectorImpl.c
+
+LOCAL_MODULE := libshim_vectorimpl
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
 # camera
 
 include $(CLEAR_VARS)
@@ -10,10 +22,9 @@ LOCAL_SRC_FILES := \
      ui/GraphicBufferAllocator.cpp \
      ui/GraphicBufferMapper.cpp \
      MemoryHeapPmem.cpp \
-     MemoryBase.c \
-     VectorImpl.c
+     MemoryBase.c
 
-LOCAL_SHARED_LIBRARIES := liblog libcutils libhardware libui libgui libbinder libutils libsync
+LOCAL_SHARED_LIBRARIES := liblog libcutils libhardware libui libgui libbinder libutils libsync libshim_vectorimpl
 LOCAL_MODULE := libshim_camera
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
